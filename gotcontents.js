8e7d9f7be662e8356cffafc90d9265ae1d8e717c
梁琰沁 <1780304097@qq.com>
2018-04-03 23:19:13 +0800

    reload

--- gotcontents.js
+++ gotcontents.js
@@ -1,4 +1,4 @@
-let obj={
+{
 	362:{
 		Intro:"House Stark of Winterfell is one of the Great Houses of Westeros and the principal noble house of the north. In days of old they ruled as Kings of Winter, but since Aegon's Conquest they have been Wardens of the North and ruled as Lords of Winterfell. Their seat,Winterfell, is an ancient castle renowned for its strength.Their sigil is a grey direwolf racing across a field of white. Their words are \"Winter is Coming\", one of only a few house mottoes to be a warning rather than a boast.[1] Several of the POV characters of A Song of Ice and Fire are members of House StarkAside from the Karstarks of Karhold, the Starks of Winterfell may have other, distant relatives in the north. White Harbor and Barrowton are considered to be likely options.[2] Some younger Starks have also held vassal holdfasts for the lords of Winterfell.[3]",
 		Culture:{
@@ -19,6 +19,3 @@ let obj={
 		}
 	}
 }
-
-let stringify=(a=obj)=>JSON.stringify(a)
-stringify()
